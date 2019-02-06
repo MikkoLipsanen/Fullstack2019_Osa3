ef03251d967658b0d1f254e94a9a14d62cd1c12f
Mikko Lipsanen <mikko.lipsanen@helsinki.fi>
2019-02-06 21:58:56 +0200

    Tehtävä 3.10 b

--- build/precache-manifest.e4da61caaa17bea602830e5273de1ee2.js
+++ build/precache-manifest.e4da61caaa17bea602830e5273de1ee2.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "627e578113109accfb48",
-    "url": "/static/js/main.627e5781.chunk.js"
+    "revision": "9ee6dc8094060c438922",
+    "url": "/static/js/main.9ee6dc80.chunk.js"
   },
   {
     "revision": "23feea3aedfdbea1a84a",
     "url": "/static/js/1.23feea3a.chunk.js"
   },
   {
-    "revision": "627e578113109accfb48",
+    "revision": "9ee6dc8094060c438922",
     "url": "/static/css/main.4e5c6eb1.chunk.css"
   },
   {
-    "revision": "51a13eb8bd58a87012034e53d1166040",
+    "revision": "2293ede2a092acc08fd48dfa6a39de67",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.684c56a834de56651d823424cd801200.js"
+  "/precache-manifest.e4da61caaa17bea602830e5273de1ee2.js"
 );
 
 workbox.clientsClaim();
